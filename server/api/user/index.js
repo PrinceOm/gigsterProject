72b4cc4273dd8cf4f54efa2bdb264fb1b3532f72
Omneet Lachhar <omneetlachhar@gmail.com>
2016-12-20 13:28:27 -0800

    Add checkuser and auth for requests

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const controller = require('./user.controller');
+const { isAuthenticated } = require('../auth/auth.service');
 
 const users = express.Router();
 
 users.post('/', controller.create);
+
+users.get('/me', isAuthenticated, controller.me);
 
 module.exports = users;
