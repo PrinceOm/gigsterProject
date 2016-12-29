6ced012c3863ae2466fb3d82d845cb500e790a02
Omneet Lachhar <omneetlachhar@gmail.com>
2016-12-29 14:07:07 -0800

    Fix extra http in environment

--- client/modules/environment.js
+++ client/modules/environment.js
@@ -1,3 +1,3 @@
 export const PROTOCOL = 'http://';
-export const DOMAIN = 'http://ec2-54-164-31-6.compute-1.amazonaws.com:3000';
+export const DOMAIN = 'ec2-54-164-31-6.compute-1.amazonaws.com:3000';
 export const BASE_API_URL = `${PROTOCOL}${DOMAIN}`;
